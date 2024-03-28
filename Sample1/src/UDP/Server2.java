f21f47807205560cd8d87dd63909e477d4c3733e
DangTrinhCSE2021 <116567551+DangTrinhCSE2021@users.noreply.github.com>
2024-03-28 10:44:20 +0700

    Update Server2.java

--- Sample1/src/UDP/Server2.java
+++ Sample1/src/UDP/Server2.java
@@ -1,62 +1,50 @@
 package UDP;
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
-public class Server2 {
-    public static void main(String args[]) {
-        DatagramSocket serverSocket = null;
-        try {
-            serverSocket = new DatagramSocket(9876);
-
-            byte[] receiveData = new byte[1024];
-            byte[] sendData;
-
-            while (true) {
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                serverSocket.receive(receivePacket);
-
-                ByteArrayInputStream inputStream = new ByteArrayInputStream(receivePacket.getData());
-                DataInputStream dataInputStream = new DataInputStream(inputStream);
-                int a = dataInputStream.readInt();
-                int b = dataInputStream.readInt();
-                int c = dataInputStream.readInt();
-
-                int lcm = calculateLCM(a, b, c);
-
-                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-                dataOutputStream.writeInt(lcm);
-
-                sendData = outputStream.toByteArray();
-
-                InetAddress IPAddress = receivePacket.getAddress();
-                int port = receivePacket.getPort();
-                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-                serverSocket.send(sendPacket);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (serverSocket != null)
-                serverSocket.close();
+public class UDP_Server_2 {
+     static int BSCNN (int a, int b, int c)
+    {
+        int i=a;
+      while (!(i%a==0 && i%b==0 && i%c==0))
+        {
+         i++;
         }
+      return i;
+    }
+    static void gui(String data, InetAddress add, int port, DatagramSocket s) throws IOException
+    {
+     byte buff[] = data.getBytes();
+     DatagramPacket p = new DatagramPacket(buff,buff.length,add,port);
+     s.send(p);
+    }
+    static DatagramPacket nhan(DatagramSocket s) throws IOException
+    {
+     byte buff[]= new byte[256];
+     DatagramPacket p = new DatagramPacket(buff,buff.length);
+     s.receive(p);
+     return p;
+    }
+    public static void main(String[] s) throws IOException
+    {
+     DatagramSocket sv = new DatagramSocket(1234);
+     DatagramPacket p1 = nhan(sv);
+     String a = new String(p1.getData()).trim();
+     int so1 = Integer.parseInt(a);
+     DatagramPacket p2 = nhan(sv);
+     String b = new String(p2.getData()).trim();
+     int so2 = Integer.parseInt(b);
+     DatagramPacket p3 = nhan(sv);
+     String c = new String(p3.getData()).trim();
+     int so3 = Integer.parseInt(c);
+     int BSC = BSCNN(so1,so2,so3);
+     gui(String.valueOf(BSC),p1.getAddress(),p1.getPort(),sv);
+     sv.close();
+    }
     
 }
-
-private static int calculateLCM(int a, int b, int c) {
-    return lcm(lcm(a, b), c);
-}
-
-private static int lcm(int a, int b) {
-    return a * (b / gcd(a, b));
-}
-
-private static int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
     return a;
 }
 }
