fbab5d5dca2f2679c5edfc3db0cb83dae21e1c9d
DangTrinhCSE2021 <116567551+DangTrinhCSE2021@users.noreply.github.com>
2024-03-28 10:42:39 +0700

    Update Client2.java

--- Sample1/src/UDP/Client2.java
+++ Sample1/src/UDP/Client2.java
@@ -1,45 +1,42 @@
 package UDP;
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
 
-public class Client2 {
-    public static void main(String args[]) {
-        DatagramSocket clientSocket = null;
-        try {
-            clientSocket = new DatagramSocket();
-            InetAddress IPAddress = InetAddress.getByName("localhost");
-
-            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("Enter three integers (a, b, c) separated by space: ");
-            String input = inFromUser.readLine();
-            String[] numbers = input.split(" ");
-            int a = Integer.parseInt(numbers[0]);
-            int b = Integer.parseInt(numbers[1]);
-            int c = Integer.parseInt(numbers[2]);
-
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-            dataOutputStream.writeInt(a);
-            dataOutputStream.writeInt(b);
-            dataOutputStream.writeInt(c);
-
-            byte[] sendData = outputStream.toByteArray();
-
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-            clientSocket.send(sendPacket);
-
-            byte[] receiveData = new byte[1024];
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            clientSocket.receive(receivePacket);
-
-            DataInputStream dataInputStream = new DataInputStream(new ByteArrayInputStream(receivePacket.getData()));
-            int lcm = dataInputStream.readInt();
-            System.out.println("Least Common Multiple received from Server: " + lcm);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (clientSocket != null)
-                clientSocket.close();
-        }
+public class UDP_Client_2 {
+    static void gui(String data, String add, int port, DatagramSocket s) throws IOException
+    {
+     byte buff[] = data.getBytes();
+     InetAddress add_sv = InetAddress.getByName(add);
+     DatagramPacket p = new DatagramPacket(buff,buff.length,add_sv,port);
+     s.send(p);
     }
+    static String nhan(DatagramSocket s) throws IOException
+    {
+     byte buff[]= new byte[256];
+     DatagramPacket p = new DatagramPacket(buff,buff.length);
+     s.receive(p);
+     String data = new String(p.getData()).trim();
+     return data;
+    }
+    public static void main(String[] s) throws IOException
+    {
+     DatagramSocket cl = new DatagramSocket();
+     Scanner bp = new Scanner(System.in);
+     System.out.print("Nhap so 1:");
+     int so1 = bp.nextInt();
+     gui(String.valueOf(so1),"localhost",1234,cl);
+     System.out.print("Nhap so 2:");
+     int so2 = bp.nextInt();
+     gui(String.valueOf(so2),"localhost",1234,cl);
+     System.out.print("Nhap so 3:");
+     int so3 = bp.nextInt();
+     gui(String.valueOf(so3),"localhost",1234,cl);
+     String BSCNN = nhan(cl);
+     System.out.println("BSCNN cua 3 so la:"+BSCNN);
+     cl.close();
+    } 
+    
 }
