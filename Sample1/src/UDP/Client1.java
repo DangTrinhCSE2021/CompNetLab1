f0bbd6ea72bb0dfd0c14aca7ddbdf2226d2d5047
DangTrinhCSE2021 <116567551+DangTrinhCSE2021@users.noreply.github.com>
2024-03-28 10:39:17 +0700

    Update Client1.java

--- Sample1/src/UDP/Client1.java
+++ Sample1/src/UDP/Client1.java
@@ -1,26 +1,37 @@
 package UDP;
-import java.io.*;
-import java.net.*;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.Scanner;
 
-public class Client1 {
-    public static void main(String[] args) {
-        
-try{
-    Socket s = new Socket("localhost",1234);
-    BufferedReader Network_in = new BufferedReader(new 
-    InputStreamReader(s.getInputStream()));
-    BufferedWriter Network_out = new BufferedWriter(new 
-    OutputStreamWriter(s.getOutputStream()));
-    Scanner keyboard = new Scanner(System.in);
-    System.out.println("Please input a string:");
-    String data = keyboard.nextLine();
-    Network_out.write(data+"\n");
-    Network_out.flush();
-    String result = Network_in.readLine();
-    System.out.println("Data from Server:"+result);
-    s.close();
-    keyboard.close();
-    }catch(Exception e){}
+public class UDP_Client_1 {
+    public static void main(String s[])
+    {
+        try{
+     DatagramSocket cl = new DatagramSocket();
+    
+     Scanner sc = new Scanner(System.in);    
+     System.out.println("Please input a string:");
+     String st= sc.nextLine();
+     
+     
+    
+     byte buff[] = st.getBytes();
+     InetAddress addsv = InetAddress.getByName("localhost");
+     DatagramPacket p = new DatagramPacket(buff,buff.length,addsv,1234);
+     cl.send(p);
+          
+     byte buff2[] = new byte[256];
+     DatagramPacket l = new DatagramPacket(buff2,buff2.length);
+     cl.receive(l);
+
+     String data = new String(l.getData()).trim();
+     System.out.println("Data from Server:"+data);
+     
+     cl.close();
+        } catch(IOException e){}
     }
+    
 }
